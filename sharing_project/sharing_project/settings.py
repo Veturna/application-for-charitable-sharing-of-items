4354d71a0533dc5997ddde05418df4efda6ca1d5
Veturna <dpullo@outlook.com>
2023-03-14 13:16:17 +0100

    create database, edit the settings.py and make 1st migration

--- sharing_project/sharing_project/settings.py
+++ sharing_project/sharing_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'donatation_app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'sharing_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'charity_donation',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+        'HOST': '127.0.0.1',
     }
 }
 
