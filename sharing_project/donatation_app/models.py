acee783182dab8a3dcab49034a65333e58fd45ed
Veturna <dpullo@outlook.com>
2023-03-14 18:16:44 +0100

    create donation model

--- sharing_project/donatation_app/models.py
+++ sharing_project/donatation_app/models.py
@@ -12,8 +12,24 @@ TYPE = (
 class Category(models.Model):
     name = models.CharField(max_length=64, unique=True)
 
+
 class Institution(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField()
     type = models.IntegerField(choices=TYPE, default=4)
     category = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=125)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=64)
+    zip_code = models.IntegerField()
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=True)
+
