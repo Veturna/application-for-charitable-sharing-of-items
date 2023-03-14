5f65a57527d2682f526abad6528e3509597399a8
Veturna <dpullo@outlook.com>
2023-03-14 14:00:13 +0100

    create login and register views

--- sharing_project/donation_app/views.py
+++ sharing_project/donation_app/views.py
@@ -6,6 +6,17 @@ class LandingPage(View):
     def get(self, request):
         return render(request, "index.html")
 
+
 class AddDonation(View):
-    def get(self, requst):
-        return render(requst, "form.html")
+    def get(self, request):
+        return render(request, "form.html")
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, "login.html")
+
+
+class Register(View):
+    def get(self, request):
+        return render(request, "register.html")
