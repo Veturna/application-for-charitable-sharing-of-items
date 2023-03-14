1ae9fd088772660bd62de91562b2e7d0273fe3da
Veturna <dpullo@outlook.com>
2023-03-14 18:02:33 +0100

    create category and institution models

--- sharing_project/donatation_app/models.py
+++ sharing_project/donatation_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+TYPE = (
+    (1, "fundacja"),
+    (2, "organizacja pozarządowa"),
+    (3, "zbiórka lokalna"),
+    (4, "fundacja")
+)
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPE, default=4)
+    category = models.ManyToManyField(Category)
