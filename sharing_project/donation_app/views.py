98cbe73401b3d44e68ed17586bb063392b9fd1e4
Veturna <dpullo@outlook.com>
2023-03-14 13:58:10 +0100

    create landingpage and addonation views

--- sharing_project/donation_app/views.py
+++ sharing_project/donation_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LandingPage(View):
+    def get(self, request):
+        return render(request, "index.html")
+
+class AddDonation(View):
+    def get(self, requst):
+        return render(requst, "form.html")
