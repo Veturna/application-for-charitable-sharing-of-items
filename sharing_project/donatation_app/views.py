39b014d121af8443f817958a7b6484e478f39f50
Veturna <dpullo@outlook.com>
2023-03-17 14:23:52 +0100

    add login method and registration

--- sharing_project/donatation_app/views.py
+++ sharing_project/donatation_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from django.db.models import Sum
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
 from donatation_app.models import Donation, Institution
 
@@ -8,27 +11,23 @@ from donatation_app.models import Donation, Institution
 class LandingPage(View):
     def get(self, request):
         donation = Donation.objects.all()
-        #total_donations = donation.aggregate(Sum('quantity'))['quantity__sum'] or 0
+
         total_donations = 0
         for d in donation:
             if d.quantity == 0:
                 total_donations = 0
             total_donations += d.quantity
 
-        #supported_institutions = donation.distinct('institution').count()
         supported_institutions = len([d.institution for d in donation])
 
         foundations = Institution.objects.filter(type=1)
         ngos = Institution.objects.filter(type=2)
         locals = Institution.objects.filter(type=3)
 
-
-
-
-
         return render(request, "index.html", {"total_donations": total_donations,
                                               "supported_institutions": supported_institutions,
-                                              "foundations": foundations, "ngos": ngos, "locals": locals }
+                                              "foundations": foundations, "ngos": ngos, "locals": locals
+                                              }
                       )
 
 
@@ -41,7 +40,38 @@ class Login(View):
     def get(self, request):
         return render(request, "login.html")
 
+    def post(self, request):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            url = reverse('landing-page')
+            return redirect(url)
+
+        url = reverse('register')
+        return redirect(url)
+
 
 class Register(View):
     def get(self, request):
         return render(request, "register.html")
+
+    def post(self, request):
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        if password2 == password:
+                user = User.objects.create_user(first_name = name, last_name = surname, username = email, password = password)
+                user.save()
+
+                url = reverse('login')
+                return redirect(url)
+
+        return render(request, 'register.html')
+
+
